32b2fa72a0a9700d6a473179486fa996ff8de7e1
ronalshuvamboruah <87542567+ron60bro@users.noreply.github.com>
2023-09-27 15:19:20 +0530

    authentication and aws backend service

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyfullstack6bb67b4a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "@aws-amplify/ui-react"
+import logo from "./logo.svg"
+import{
+  withAuthenticator,
+  Button,
+  Heading,
+  Image,
+  View,
+  Card
+}from "@aws-amplify/ui-react"
+function App({signOut}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Updating the App
-        </a>
-      </header>
-    </div>
-  );
+   <View>
+    <Card>
+      <Image src={logo} className="App-logo" alt="logo"/>
+      <Heading level={1}>We have now auth</Heading>
+    </Card>
+    <Button onClick={signOut}>Sign Out</Button>
+   </View>
+  )
 }
 
-export default App;
+export default withAuthenticator(App)
